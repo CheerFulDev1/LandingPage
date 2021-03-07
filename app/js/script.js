2f08b7c2e256b6529ce121e2fd7f6aeee2be0966
CheerFulDev1 <loukilihz@gmail.com>
2021-03-07 16:34:19 +0100

    added mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,25 @@
+const body = document.querySelector('body');
 const bntHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeelmnts = document.querySelectorAll('.has-fade');
 
-bntHamburger.addEventListener('click', function(){
-    if(header.classList.contains('open')){ //close menu
+bntHamburger.addEventListener('click', function () {
+    if (header.classList.contains('open')) { //close menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
-    }else{ //open menu
+        fadeelmnts.forEach(function (element) {
+            element.classList.add('has-fade');
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    } else { //open menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeelmnts.forEach(function (element) {
+            element.classList.remove('has-fade');
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
     }
 })
