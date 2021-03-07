3b228bb513826f087ffcd3dda965c3e47fb4807f
CheerFulDev1 <loukilihz@gmail.com>
2021-03-07 15:23:51 +0100

    fade-in fade-out menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log("hi there");
+const bntHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+bntHamburger.addEventListener('click', function(){
+    if(header.classList.contains('open')){ //close menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }else{ //open menu
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }
+})
